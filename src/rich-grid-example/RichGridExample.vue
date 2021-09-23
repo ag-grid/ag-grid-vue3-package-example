6b10e5b213844f333762937f432085949e4bf3a8
Stephen Cooper <scooperdev@gmail.com>
2021-09-23 12:06:26 +0100

    AG-5783 Remove 'dateComponentFramework'

--- src/rich-grid-example/RichGridExample.vue
+++ src/rich-grid-example/RichGridExample.vue
@@ -282,7 +282,7 @@ export default {
   },
   beforeMount() {
     this.gridOptions = {};
-    this.gridOptions.dateComponentFramework = DateComponent;
+    this.gridOptions.frameworkComponents = { agDateInput: DateComponent };
     this.createRowData();
     this.createColumnDefs();
     this.showGrid = true;
